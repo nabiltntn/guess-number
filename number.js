ea8dd30b1a59292d4dd9b715cec8ecad0ac301a0
Mohamed Nabil Zouabi <zouabi.nabil@live.com>
2015-11-17 00:34:49 +0100

    Final commit

--- number.js
+++ number.js
@@ -1,23 +1,20 @@
 if (Meteor.isClient) {
 
   Template.hello.onRendered(() => {
-    let maxValue = 942240
+    let maxValue = 950000
     let currentValue = 10
     let found = false
 
-    
     do {
       currentValue += 1;
       found = NumberGame.checkNumber(currentValue)
     } while (currentValue <= maxValue && found == false);
-
+    
     if(found) {
       console.log(`Found !!! ${currentValue}`)
     } else {
       console.log(`Not Found !!! ${currentValue}`)
     }
 
-
   })
-
 }
